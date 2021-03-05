fc34b41437f8d585b3a395ef1a304effe4d9b100
indera <mehrdad.zadehebrahim@gmail.com>
2021-03-05 07:29:29 +0330

    Add files via upload
    Use this code for comparing two big and  bad organized excel files.

---
+++ compare-excel-files.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+# Read both Excel files
+file1 = pd.read_excel("file1.xlsx", na_values=['NA'])
+file2 = pd.read_excel("file2.xlsx", na_values=['NA'])
+
+
+df2 = file1
+df1 = file2
+
+
+
+res = df1[df1['samecolname'].isin(df2['samecolname'].unique())]
+                   
+res2 = df2[df2['samecolname'].isin(df1['samecolname'].unique())]               
+
+res.to_excel('diff1-insecond-but-not-in-first.xlsx',index=False)
+res2.to_excel('diff2-in-first-not-in-second.xlsx',index=False)
